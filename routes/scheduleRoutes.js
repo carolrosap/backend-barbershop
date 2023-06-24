982bfe5edb62173692fefb1c6f7141cba1fe14cd
Caroline Rosa <cpauladarosa@gmail.com>
2023-06-24 03:33:39 -0300

    cors added

--- routes/scheduleRoutes.js
+++ routes/scheduleRoutes.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const ScheduleController = require('../controllers/ScheduleController');
+const scheduleController = new ScheduleController();
 
-// Definir endpoints
-router.get('/', ScheduleController.getAllSchedules);
-router.get('/:id', ScheduleController.getScheduleById);
-router.post('/', ScheduleController.createSchedule);
-router.put('/:id', ScheduleController.updateSchedule);
-router.delete('/:id', ScheduleController.deleteSchedule);
+router.get('/', scheduleController.getAllSchedules);
+//router.post('/', serviceController.createService);
+
 
 module.exports = router;
